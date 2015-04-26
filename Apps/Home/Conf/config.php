c7e22615139473124addc2d9c5303d39159509e7
Sunshinewyf <2632807692@qq.com>
2015-04-26 15:57:28 +0800

    Update config.php

--- Apps/Home/Conf/config.php
+++ Apps/Home/Conf/config.php
@@ -6,7 +6,7 @@ return array(
 		'__UPLOADS__' => '/blog/Uploads',
 		'SHOW_PAGE_TRACE'=>true,//开启页面Trace
         '__PUBLIC__' => '/blog/Public',
-        'DB_DSN' => 'mysql://root:@localhost:3306/blog#utf8',
+        'DB_DSN' => 'mysql://root:123456@localhost:3306/blog#utf8',
         'DB_PREFIX' => 'bl_',
 );
 ?>
